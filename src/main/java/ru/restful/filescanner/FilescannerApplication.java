842099e380641a2961a5051e621deef98cc7d743
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-03-25 22:47:01 +0300

    initial commit

---
+++ src/main/java/ru/restful/filescanner/FilescannerApplication.java
@@ -0,0 +1,13 @@
+package ru.restful.filescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilescannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilescannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/restful/filescanner/FilescannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.restful.filescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilescannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
