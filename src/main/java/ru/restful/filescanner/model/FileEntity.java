a644ae25b01612a8f3de4f72d1c97dc1a6ec03f4
Aleksandr Pronichev <alexander.pronichew@yandex.ru>
2025-03-25 23:43:25 +0300

    add model package

---
+++ src/main/java/ru/restful/filescanner/model/FileEntity.java
@@ -0,0 +1,47 @@
+package ru.restful.filescanner.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String fileName;
+    private String filePath;
+
+    public FileEntity() {}
+
+    public FileEntity(String fileName, String filePath) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
